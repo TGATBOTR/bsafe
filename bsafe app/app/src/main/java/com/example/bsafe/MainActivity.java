d541d9a3e3e13870b231f41504583df6827275a2
DESKTOP-D153I8B\jatin <jatin.yadav@favouritetable.com>
2022-02-20 17:43:42 +0000

    home page as per prototype design
    Very basic at the moment, simple buttons made in MS paint.
    Middle card shows allergy, buttons either side to show other allergies - will change to slide at later point as needs more work.
    Added a splash screen which lasts 3 seconds though the image doesnt appear.
    Also added the "add allergy button" however no functionality behind it yet, that is next - need to find out how to create new allergies

--- bsafe app/app/src/main/java/com/example/bsafe/MainActivity.java
+++ bsafe app/app/src/main/java/com/example/bsafe/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.bsafe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -10,6 +12,8 @@ import com.example.bsafe.Auth.Session;
 import com.example.bsafe.Database.Daos.AllergyDao;
 import com.example.bsafe.Database.Models.Allergy;
 import com.example.bsafe.Database.Models.User;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -24,23 +28,59 @@ public class MainActivity extends AppCompatActivity {
 
     @Inject
     public AllergyDao allergyDao;
+    private List <Allergy> allergies;
+    private boolean retrieved = false;
+
+
+    private TextView englishAllergyName;
+    private int currentAllergy = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
+        englishAllergyName = findViewById(R.id.textView3);
 
         User currentUser = session.getUser();
 
-        TextView label = (TextView) findViewById(R.id.label);
-
-        label.setText("Hello, " + currentUser.firstName);
+//        TextView label = (TextView) findViewById(R.id.label);
+//        label.setText("Hello, " + currentUser.firstName);
 
         Thread t = new Thread() {
             public void run() {
-                List<Allergy> allergies = allergyDao.getUserAllergies(currentUser.uid);
+                allergies = allergyDao.getUserAllergies(currentUser.uid);
+                retrieved = true;
             }
         };
+        t.start();
 
+        while (!retrieved){
+        }
+        setAllergyText();
     }
+
+    public void shift( View view){
+        int shift;
+        if (view.getId() == R.id.right){
+            shift = 1;
+        } else {
+            shift = -1;
+        }
+        if (allergies.size() != 0) {
+            currentAllergy += shift;
+            currentAllergy = currentAllergy % allergies.size();
+            setAllergyText();
+        }
+    }
+
+    private void setAllergyText(){
+        String text;
+        if(allergies.size() != 0){
+            text = allergies.get(currentAllergy).name;
+        } else {
+            text = "none";
+        }
+        englishAllergyName.setText(text);
+    }
+
 }
