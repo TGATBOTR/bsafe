e74c72446fda83f8e24cc32036f261f7433d1c08
Jay Rabjohns <52382074+jayrabjohns@users.noreply.github.com>
2022-03-21 13:47:37 +0000

    QR codes now show entered allergies

--- bsafe app/app/src/main/java/com/example/bsafe/QRGenerator.java
+++ bsafe app/app/src/main/java/com/example/bsafe/QRGenerator.java
@@ -1,38 +1,69 @@
 package com.example.bsafe;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.bsafe.Auth.Session;
+import com.example.bsafe.Database.Daos.AllergyDao;
+import com.example.bsafe.Database.Models.Allergy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
 
 import androidmads.library.qrgenearator.QRGContents;
 import androidmads.library.qrgenearator.QRGEncoder;
+import dagger.hilt.android.AndroidEntryPoint;
 
+@AndroidEntryPoint
 public class QRGenerator extends AppCompatActivity {
-    String qrValue;
-    ImageView qrImage;
 
+    @Inject
+    public Session session;
+    @Inject
+    public AllergyDao allergyDao;
+
+    private final int qrCodeSize = 500;
+    private final List<Allergy> allergies = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_qr_code);
 
-        qrImage = findViewById(R.id.qrContainer);
-        qrValue = "I am allergic to Luca";
+        int uid = session.getUser().uid;
+        Thread getAllergiesThread = new Thread(() -> allergies.addAll(allergyDao.getUserAllergies(uid)));
+        getAllergiesThread.start();
+        try { getAllergiesThread.join(); } catch (InterruptedException e) { e.printStackTrace(); }
 
-        QRGEncoder qrgEncoder = new QRGEncoder(qrValue, null, QRGContents.Type.TEXT,500);
-        try {
-            // Getting QR-Code as Bitmap
-            Bitmap bitmap = qrgEncoder.getBitmap();
-            // Setting Bitmap to ImageView
-            qrImage.setImageBitmap(bitmap);
-        } catch (NullPointerException e) {
-            e.printStackTrace();
+        String qrValue = getQRContentFromAllergies(allergies);
+        QRGEncoder qrgEncoder = new QRGEncoder(qrValue, null, QRGContents.Type.TEXT, qrCodeSize);
+
+        Bitmap bitmap = qrgEncoder.getBitmap();
+        if (bitmap == null)
+        {
+            // TODO: replace with placeholder image for missing QR code
+            bitmap = Bitmap.createBitmap(qrCodeSize, qrCodeSize, Bitmap.Config.ARGB_8888);
         }
 
+        ImageView qrImage = findViewById(R.id.qrContainer);
+        qrImage.setImageBitmap(bitmap);
+    }
+
+    @NonNull
+    private String getQRContentFromAllergies(@NonNull List<Allergy> allergies)
+    {
+        StringBuilder qrValue = new StringBuilder();
+        for (Allergy allergy: allergies)
+        {
+            qrValue.append(String.format("%s | %s | %s", allergy.name, allergy.scale, allergy.symptoms));
+        }
+
+        return qrValue.toString();
     }
 }
